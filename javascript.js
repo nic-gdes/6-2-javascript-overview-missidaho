c41bdccded15924f26d6107a48dd0d4482803263
Madeleine <mgleahy@nic.edu>
2023-09-28 15:54:03 -0700

    finished

--- javascript.js
+++ javascript.js
@@ -1,15 +1,64 @@
 // 1.1 Assignment: Where do you put JavaScript?
+// at the end in your body
 
 // 1.2 Assignment: Variables
-var dudesname="Madeleine";
-console.log (dudesname);
+var dudesname = "Madeleine";
+console.log(dudesname);
 
 // 1.3 Assignment: Variable Declaration & Initialization
+var a;
+// this is a undefinied
+var b = "dog";
+// this is a String
+var c = 1;
+// this is a number
+var d = false;
+// this is a boolean
+console.log(typeof (a));
+console.log(typeof (b));
+console.log(typeof (c));
+console.log(typeof (d));
+
+
+
 
 // 1.4 Assignment: Complete your statements
+x = 2;
+y = 3;
+z = x + y;
+// this is a number
+console.log((z = x + y));
 
 // 1.5 Assignment: Brackets, Braces and Parentheses
+// supply parameters x and y
+function myParams(x, y) {
+    return x + y;
+}
+//group expressions to control the order of execution
+var a = (3 + 2) * 7; // returns a = 35
+//execute functions
+myParams(2, 3); // returns 5
+
+var a = function () {
+    alert('Statement 1');
+    alert('Statement 2');
+}
+console.log(a)
 
 // 1.6 Assignment: Data Types
+var a = "Madeleine";
+var b = 19;
+var c = true;
+console.log(a)
+console.log(b)
+console.log(c)
+
+
 
 // 1.7 Assignment: Practicing JavaScript basics
+var a = 4;
+var b = 3;
+var c = 5;
+var d = b * a / 2;
+console.log = (d);
+
